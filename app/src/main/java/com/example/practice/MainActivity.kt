047dc8e1b1c06a10a04bb241e07151a1abf9285d
karan kumar baitha <116413125+karankumar544@users.noreply.github.com>
2023-05-12 18:37:03 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.practice
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
